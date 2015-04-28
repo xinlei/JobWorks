b57de49a89a511950aec748bc7440ddfff2641cb
xinlei <wang.xinlei15@gmail.com>
2015-04-27 23:54:07 -0700

    basic querying

--- JS/search.js
+++ JS/search.js
@@ -10,15 +10,31 @@ $('document').ready(function() {
     }
 
     $("#submitSearch").click(function() { 
-        alert("Go");
+        var query = new Parse.Query(Parse.User);
+
+        if($("#query_firstname").val()){
+            query.equalTo("f_name", $("#query_firstname").val());
+        }
+        if($("#query_lastname").val()){
+            query.equalTo("l_name", $("#query_lastname").val());
+        }
+        if($("#query_email").val()){
+            query.equalTo("l_name", $("#query_email").val());
+        }
+        query.find({
+            success: function(results){
+                alert("Successfully retrieved " + results.length + " users.");
+                for(var i = 0; i < results.length; i++){
+                    // build table
+                }    
+            },
+            error: function(error){
+                alert("Error: " + error.code + " " + error.message);
+            } 
+        }); 
     });
 
      $("#submitShowAdvancedSearch").click(function() { 
         alert("Advanced");
     });
-});
-
-$("#query-form input[type=submit]").click(function() {
-    $("input[type=submit]", $(this).parents("#query-form")).removeAttr("clicked");
-    $(this).attr("clicked", "true");
 });
