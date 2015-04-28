e12bb67212e8a29ccb7d6968aa6e5872638e919c
xinlei <wang.xinlei15@gmail.com>
2015-04-27 22:00:20 -0700

    testing/debugging

--- JS/P1.js
+++ JS/P1.js
@@ -1,25 +1,38 @@
-<script src="http://www.parsecdn.com/js/parse-1.4.2.min.js"></script>
-<script language="JavaScript">
-    Parse.initialize("O2zmBbmKr3hO6JJC0o5CxswEsEUxwIGpvviyYjsc", "Xexg4iA4TwdMu4hy6RNrbgrFEkLRTwMjmhplMGya");
-    $('document').ready(function() {
-        var currentUser = Parse.User.current();
-        if (currentUser) {
-            
-        } else {
-            // show the login page if not logged in
-            window.location.href = "login.html";
+Parse.initialize("O2zmBbmKr3hO6JJC0o5CxswEsEUxwIGpvviyYjsc", "Xexg4iA4TwdMu4hy6RNrbgrFEkLRTwMjmhplMGya");
+$('document').ready(function() {
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        
+    } else {
+        // show the login page if not logged in
+        window.location.href = "login.html";
+    }
+    console.log(currentUser);
+});
+
+$( '#next_button' ).click(function() {
+    alert('test');
+    //console.log(currentUser);
+    var f_name = $('#f_name').val();
+    var l_name = $('#l_name').val();
+    var email = $('#email').val();
+    //var phone = $('#phone').val();
+    //var ssn = $('#ssn').val();
+
+    var currentUser = Parse.User.current();
+    currentUser.set('f_name', f_name);
+    currentUser.set('l_name', l_name);
+    currentUser.set('email', email);
+    //currentUser.set('phone', phone);
+    //currentUser.set('ssn', ssn);
+
+    console.log(currentUser);
+    currentUser.save(null, {
+        success: function(currentUser) {
+            alert('currentUser values updated');
+        },
+        error: function(currentUser, error) {
+            alert('currentUser values failed to update, with error code: ' + error.message);
         }
-    });
-    $('#next_button').click(function() {
-        var f_name = $('#f_name').value();
-        var l_name = $('#l_name').value();
-        var email = $('#email').value();
-        var phone = $('#phone').value();
-        var ssn = $('#ssn').value();
-        currentUser.set('f_name',f_name);
-        currentUser.set('l_name',l_name);
-        currentUser.set('email',email);
-        currentUser.set('phone',phone);
-        currentUser.set('ssn',ssn);
-    });
-</script>
+    })
+});
---
+++ JS/search.js
@@ -0,0 +1,11 @@
+Parse.initialize("O2zmBbmKr3hO6JJC0o5CxswEsEUxwIGpvviyYjsc", "Xexg4iA4TwdMu4hy6RNrbgrFEkLRTwMjmhplMGya");
+    
+$('document').ready(function() {
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        
+    } else {
+        // show the login page if not logged in
+        window.location.href = "login.html";
+    }
+});
