9627df6ffdb676dd7466be0e117c97cf39af80d1
xinlei <wang.xinlei15@gmail.com>
2015-05-24 00:41:12 -0500

    complete implementation of P2

--- JS/P2.js
+++ JS/P2.js
@@ -1,113 +1,200 @@
+// Removed the work experience at given index
+function clean(i){
+    index = i + 1; 
+    $('#emp'+index).val("");
+    $('#addr'+index).val("");
+    $('#title'+index).val("");
+    $('#time'+index).val("");
+    $('#hours'+index).val("");
+    $('#super'+index).val("");
+    $('#resp'+index).val("");
+    $('#r_leave'+index).val("");
+}
+
+// Load all available employment experience from parse
+function loadEmployers(currentUser, employers, toggle){
+    var query = new Parse.Query("Employer");
+    query.equalTo("createdBy", currentUser);   
+
+    query.find({
+        success: function(results) {
+            alert("Successfully retrieved " + results.length + " scores.");
+            for (var i = 0; i < 4; i++) {
+                var index = i + 1;
+
+                if(i < results.length){ 
+                    console.log(results.length);
+                    var employer = results[i];
+                    employers.push(employer); 
+                    $('#emp'+index).val(employer.get("emp"));
+                    $('#addr'+index).val(employer.get("addr"));
+                    $('#title'+index).val(employer.get("title"));
+                    $('#time'+index).val(employer.get("time"));
+                    $('#hours'+index).val(employer.get("hours"));
+                    $('#super'+index).val(employer.get("super"));
+                    $('#resp'+index).val(employer.get("resp"));
+                    $('#r_leave'+index).val(employer.get("r_leave"));
+                    $('#contact_emp'+index).val(employer.get("contact_emp"));
+
+                    // Reset toggle options
+                    if(toggle){
+                        $('#employer'+index).toggle(true);
+                        $('#add_job_button'+index).toggle(false);
+                        $('#min_job_button'+index).toggle(true);
+                    }
+
+                } else {
+                    $('#emp'+index).val("");
+                    $('#addr'+index).val("");
+                    $('#title'+index).val("");
+                    $('#time'+index).val("");
+                    $('#hours'+index).val("");
+                    $('#super'+index).val("");
+                    $('#resp'+index).val("");
+                    $('#r_leave'+index).val("");
+                    $('#contact_emp'+index).val("");
+
+                }
+                
+            }
+        },
+        error: function(error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
+
+// Update the database once the user enters his/her employment experience
+function updateEmployers(currentUser){
+    console.log(currentUser);
+    var Employer = Parse.Object.extend("Employer");
+    
+    // maximum of 4 employment entries
+    for(var i = 1; i < 5; i++){
+        var checkString = $('#emp'+i).val();
+        console.log(checkString)
+        if(checkString && checkString.length > 0){
+            var employer = new Employer();
+            var emp = $('#emp'+i).val();
+            var addr = $('#addr'+i).val();
+            var title = $('#title'+i).val();
+            var time = $('#time'+i).val();
+            var hours = $('#hours'+i).val();
+            var supervisor = $('#super'+i).val();
+            var resp = $('#resp'+i).val();
+            var r_leave = $('#r_leave'+i).val();
+            var contact_emp = $('#contact_emp'+i).val();
+            employer.set('emp',emp);
+            employer.set('addr',addr);
+            employer.set('title',title);
+            employer.set('time',time);
+            employer.set('hours',hours);
+            employer.set('super',supervisor);
+            employer.set('resp',resp);
+            employer.set('r_leave',r_leave);
+            employer.set('contact_emp',contact_emp);
+
+            // Establish relation with the current user
+            employer.set("createdBy", currentUser);
+
+            employer.save(null, {
+                success: function(currentUser) {
+                    alert('currentUser values updated');
+                },
+                error: function(currentUser, error) {
+                    alert('displaye currentUser values failed to update, with error code: ' + error.message);
+                }
+            });
+        }
+    }
+}
 
 Parse.initialize("O2zmBbmKr3hO6JJC0o5CxswEsEUxwIGpvviyYjsc", "Xexg4iA4TwdMu4hy6RNrbgrFEkLRTwMjmhplMGya");
 $('document').ready(function() {
     var currentUser = Parse.User.current();
+    var employers = []; 
     if (currentUser) {
-        
+        loadEmployers(currentUser, employers, true); 
     } else {
-        // show the login page if not logged in
         window.location.href = "login.html";
     }
-});
-//Temporarily nulled
-$('#next_button').click(function() {
-    var Employer = Parse.Object.extend("Employer");
-    //first employer
-    /*
-        Query current user for employers
-        Display Employers and give option to edit
-    */
-    var employer1 = new Employer();
-    var emp1 = $('#emp1').value();
-    var addr1 = $('#addr1').value();
-    var title1 = $('#title1').value();
-    var time1 = $('#time1').value();
-    var hours1 = $('#hours1').value();
-    var super1 = $('#super1').value();
-    var resp1 = $('#resp1').value();
-    var r_leave1 = $('#r_leave1').value();
-    var contact_emp1 = $('#contact_emp1').value();
-    employer1.set('emp',emp1);
-    employer1.set('addr',addr1);
-    employer1.set('title',title1);
-    employer1.set('time',time1);
-    employer1.set('hours',hours1);
-    employer1.set('super',super1);
-    employer1.set('resp',resp1);
-    employer1.set('r_leave',r_leave1);
-    employer1.set('contact_emp',contact_emp1);
-    //second employer
-    /*var employer2 = new Employer();
-    var emp2 = $('#emp2').value();
-    var addr2 = $('#addr2').value();
-    var title2 = $('#title2').value();
-    var time2 = $('#time2').value();
-    var hours2 = $('#hours2').value();
-    var super2 = $('#super2').value();
-    var resp2 = $('#resp2').value();
-    var r_leave2 = $('#r_leave2').value();
-    var contact_emp2 = $('#r_leave2').value();
-    employer2.set('emp',emp2);
-    employer2.set('addr',addr2);
-    employer2.set('title',title2);
-    employer2.set('time',time2);
-    employer2.set('hours',hours2);
-    employer2.set('super',super2);
-    employer2.set('resp',resp2);
-    employer2.set('r_leave',r_leave2);
-    employer2.set('contact_emp',contact_emp2);
-    //third employer
-    var employer3 = new Employer();
-    var emp3 = $('#emp3').value();
-    var addr3 = $('#addr3').value();
-    var title3 = $('#title3').value();
-    var time3 = $('#time3').value();
-    var hours3 = $('#hours3').value();
-    var super3 = $('#super3').value();
-    var resp3 = $('#resp3').value();
-    var r_leave3 = $('#r_leave3').value();
-    var contact_emp3 = $('#r_leave3').value();
-    employer3.set('emp',emp3);
-    employer3.set('addr',addr3);
-    employer3.set('title',title3);
-    employer3.set('time',time3);
-    employer3.set('hours',hours3);
-    employer3.set('super',super3);  
-    employer3.set('resp',resp3);
-    employer3.set('r_leave',r_leave3);
-    employer3.set('contact_emp',contact_emp3);
-    //fourth employer
-    var employer4 = new Employer();
-    var emp4 = $('#emp4').value();
-    var addr4 = $('#addr4').value();
-    var title4 = $('#title4').value();
-    var time4 = $('#time4').value();
-    var hours4 = $('#hours4').value();
-    var super4 = $('#super4').value();
-    var resp4 = $('#resp4').value();
-    var r_leave4 = $('#r_leave4').value();
-    var contact_emp4 = $('#r_leave4').value();
-    employer4.set('emp',emp4);
-    employer4.set('addr',addr4);
-    employer4.set('title',title4);
-    employer4.set('time',time4);
-    employer4.set('hours',hours4);
-    employer4.set('super',super4);
-    employer4.set('resp',resp4);
-    employer4.set('r_leave',r_leave4);
-    employer4.set('contact_emp',contact_emp4);
-    //set user employer attributes
-    currentUser.set('exp1', exp1);
-    currentUser.set('exp2', exp2);
-    currentUser.set('exp3', exp3);
-    currentUser.set('exp4', exp4);*/
-    employer1.save(null, {
-        success: function(currentUser) {
-            alert('currentUser values updated');
-        },
-        error: function(currentUser, error) {
-            alert('currentUser values failed to update, with error code: ' + error.message);
-        }
-    })
     
+    // Delete everything in the database and enter a fresh set of entries
+    $('#next_button').click(function() {
+        event.preventDefault();
+        Parse.Object.destroyAll(employers).then(function(success){
+            updateEmployers(currentUser); 
+        }, function(error){
+        
+        });
+    });
+
+    var next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#job_field" + next;
+        var addRemove = "#job_field" + (next);
+        next = next + 1;
+        var newIn = '<input autocomplete="off" class="input form-control" id="job_field' + next + '" name="job_field' + next + '" type="text">';
+        var newInput = $(newIn);
+        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-default remove-me" >x</button></div><div id="job_field">';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#job_field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            $('.remove-me').click(function(e){
+                e.preventDefault();
+                var fieldNum = this.id.charAt(this.id.length-1);
+                var fieldID = "#job_field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+            });
+    });
+    
+    $("#add_job_button1").click(function() {
+        $("#employer1").toggle();
+        $(this).toggle();
+        $('#min_job_button1').toggle();
+    });
+    $("#min_job_button1").click(function() {
+        $("#employer1").toggle();
+        $(this).toggle();
+        $('#add_job_button1').toggle();
+        clean(0, employers, removed);
+    });
+    $("#add_job_button2").click(function() {
+        $("#employer2").toggle();
+        $(this).toggle();
+        $('#min_job_button2').toggle();
+    });
+    $("#min_job_button2").click(function() {
+        $("#employer2").toggle();
+        $(this).toggle();
+        $('#add_job_button2').toggle();
+        clean(1, employers, removed);
+    });
+    $("#add_job_button3").click(function() {
+        $("#employer3").toggle();
+        $(this).toggle();
+        $('#min_job_button3').toggle();
+    });
+    $("#min_job_button3").click(function() {
+        $("#employer3").toggle();
+        $(this).toggle();
+        $('#add_job_button3').toggle();
+        clean(2, employers, removed);
+        });
+    $("#add_job_button4").click(function() {
+        $("#employer4").toggle();
+        $(this).toggle();
+        $('#min_job_button4').toggle();
+    });
+    $("#min_job_button4").click(function() {
+        $("#employer4").toggle();
+        $(this).toggle();
+        $('#add_job_button4').toggle();
+        clean(3, employers, removed);
+    });
 });
