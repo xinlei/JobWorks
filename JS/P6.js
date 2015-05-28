d65725d20de60deb9b37af1194f8160a576be4ad
Steven Qian <scqian@stanford.edu>
2015-05-27 23:01:38 -0700

    paragraph

--- JS/P6.js
+++ JS/P6.js
@@ -12,13 +12,25 @@ $('document').ready(function() {
 
     //Get initial value for the paragraph intro
     var f_name = currentUser.get("f_name");
+    if (!f_name) {
+        f_name = "we have not gotten your name yet"
+    }
     var hometown = currentUser.get("hometown");
+    if (!hometown) {
+        hometown = "unspecified city";
+    }
     var edu = currentUser.get("edu");
+    if (!edu) {
+        edu = "an unspecified school"
+    }
     var industry_interests = "";
     var category_interests = "";
     var start = currentUser.get("start");
+    if (!start) {
+        start = "an unspecified date";
+    }
 
-    //add industry and category interests
+    //add industry interests
     var industry_list = [];
     var construction = currentUser.get('construction');
     var office = currentUser.get('office');
@@ -28,33 +40,71 @@ $('document').ready(function() {
     var caregiving = currentUser.get('caregiving');
     var culinary = currentUser.get('culinary');
     var financial = currentUser.get('financial');
+    var counter = 0;
     if (construction.length > 0) {
         industry_interests += "construction";
+        counter++;
     }
     if (office.length > 0) {
         industry_interests += ", office";
+        counter++;
     }
     if (warehouse.length > 0) {
         industry_interests += ", warehouse";
+        counter++;
+
     }
     if (sales.length > 0) {
-        industry_interests += "sales";
+        industry_interests += ", sales";
+        counter++;
+
     }
     if (automotive.length > 0) {
-        industry_interests += "automotive";
+        industry_interests += ", automotive";
+        counter++;
+
     }
     if (caregiving.length > 0) {
-        industry_interests += "caregiving";
+        industry_interests += ", caregiving";
+        counter++;
+
     }
     if (culinary.length > 0) {
-        industry_interests += "culinary";
+        industry_interests += ", culinary";
+        counter++;
+
     }
     if (financial.length > 0) {
-        industry_interests += "financial";
+        industry_interests += ", financial";
+        counter++;
+
     }
+
+    //create the industry value
+    var last_comma_index = industry_interests.lastIndexOf(",");
+    var industry_string = "";
+    if (counter == 0) {
+        industry_string += "no specific industries"
+    }
+    else if (counter == 1) {
+        industry_string += industry_interests;
+    }
+    else if (counter == 2) {
+        industry_string = industry_interests.slice(0,last_comma_index) + " and" + industry_interests.slice(last_comma_index + 1);
+    }
+    else {
+        industry_string = industry_interests.slice(0,last_comma_index + 1) + " and" + industry_interests.slice(last_comma_index + 1);
+    }
+
 
 
     //Set values for the paragraph intro
+    $('#f_name').html(f_name);
+    $('#hometown').html(hometown);
+    $('#edu').html(edu);
+    $('#industry_interests').html(industry_string);
+    $('#start').html(start);
+
 
 
 
